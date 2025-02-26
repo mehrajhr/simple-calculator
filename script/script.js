a491f6380af347cbc64da54aedf80a7d99ec2e98
Mehraj Hasan Riaj <mehrajhasanriajcse@gmail.com>
2025-02-27 02:38:47 +0600

    add html js file with setup daisy ui and some code

---
+++ script/script.js
@@ -0,0 +1,60 @@
+document.getElementById('zero').addEventListener('click', function(event){
+    const displayText = document.getElementById("display-text");
+    displayText.innerText += '0';
+})
+document.getElementById('one').addEventListener('click', function(event){
+    const displayText = document.getElementById("display-text");
+    displayText.innerText += '1';
+})
+document.getElementById('two').addEventListener('click', function(event){
+    const displayText = document.getElementById("display-text");
+    displayText.innerText += '2';
+})
+document.getElementById('three').addEventListener('click', function(event){
+    const displayText = document.getElementById("display-text");
+    displayText.innerText += '3';
+})
+document.getElementById('four').addEventListener('click', function(event){
+    const displayText = document.getElementById("display-text");
+    displayText.innerText += '4';
+})
+document.getElementById('five').addEventListener('click', function(event){
+    const displayText = document.getElementById("display-text");
+    displayText.innerText += '5';
+})
+document.getElementById('six').addEventListener('click', function(event){
+    const displayText = document.getElementById("display-text");
+    displayText.innerText += '6';
+})
+document.getElementById('seven').addEventListener('click', function(event){
+    const displayText = document.getElementById("display-text");
+    displayText.innerText += '7';
+})
+document.getElementById('eight').addEventListener('click', function(event){
+    const displayText = document.getElementById("display-text");
+    displayText.innerText += '8';
+})
+document.getElementById('nine').addEventListener('click', function(event){
+    const displayText = document.getElementById("display-text");
+    displayText.innerText += '9';
+})
+document.getElementById('plus').addEventListener('click', function(event){
+    const displayText = document.getElementById("display-text");
+    displayText.innerText += '+';
+})
+document.getElementById('minus').addEventListener('click', function(event){
+    const displayText = document.getElementById("display-text");
+    displayText.innerText += '-';
+})
+document.getElementById('muktiple').addEventListener('click', function(event){
+    const displayText = document.getElementById("display-text");
+    displayText.innerText += '*';
+})
+document.getElementById('division').addEventListener('click', function(event){
+    const displayText = document.getElementById("display-text");
+    displayText.innerText += '/';
+})
+document.getElementById('equal').addEventListener('click', function(event){
+    const displayText = document.getElementById("display-text");
+    displayText.innerText += '=';
+})
